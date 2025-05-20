007f6af2f91e6f90c6b79317a6618384a9337e6b
Eric Holman <ericrholman@gmail.com>
2025-05-20 12:18:11 -0500

    messed around with different types of variables

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(f"Who let the dogs out")
+#Typecasting = the process of converting a cariable from one data type to another
+#          str(), int(), float(), bool()
+
+name = "Eric Holman"
+age = 20
+gpa = 3.84
+is_student = True
+
+
+name = bool(name)
+print(name)
