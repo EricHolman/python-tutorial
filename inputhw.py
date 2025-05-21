0cf05620e68cb97c630c913f40f475def5f2ed7c
Eric Holman <ericrholman@gmail.com>
2025-05-20 20:27:58 -0500

    Create inputhw.py

---
+++ inputhw.py
@@ -0,0 +1,8 @@
+#Exercise 1 Rectangle Area Calc
+
+width = float(input("What is the width of the rectangle?"))
+length = float(input("What is the length of the rectangle?"))
+
+rectangle_area = width * length
+
+print(f"The Area of your rectangle is {rectangle_area}")
